3d1585bdd17d53410fe2d193f1916da4f78e81b8
Nicolas Herrera <herreran903@gmail.com>
2024-03-08 20:31:45 -0500

    Refactor Node class and add BreadthSearch and CostSearch algorithms

--- BreadthSearch.py
+++ BreadthSearch.py
@@ -2,19 +2,28 @@ from Node import Node
 from Puzzle import Puzzle
 
 
+def pathGoal(node):
+    path = []
+    while node.parent != None:
+        path.append(node.puzzle.actualPosition)
+        node = node.parent
+    path.reverse()
+    return path
+
+
 def main():
     queueNodes = []
     index = 0
 
     puzzle = Puzzle()
-    puzzle.loadMap('Prueba1.txt')
+    puzzle.loadMap("Prueba1.txt")
     intitialNode = Node(puzzle, None, None, 0, 0)
 
     queueNodes.append(intitialNode)
 
     while True:
         if index >= len(queueNodes):
-            print('No solution found')
+            print("No solution found")
             break
 
         node = queueNodes[index]
@@ -22,19 +31,16 @@ def main():
         index += 1
 
         if node.isGoal():
-            print('Goal found')
+            print("Goal found")
             print(node.puzzle.actualPosition)
+            print("Cost: ", node.cost)
+            print("Depht: ", node.depth)
+            print("Path: ", pathGoal(node))
             break
 
         for action in possibleActions:
             newNode = node.applyAction(action)
             queueNodes.append(newNode)
-
-        
-
-
-
-
 
 
 main()
